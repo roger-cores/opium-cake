12244164b61a09839b0e4d9238bc40edfd1524fb
roger.cores <rogercores2@gmail.com>
2016-03-06 09:33:42 +0530

    Folder Structure updated

--- models/Audit.js
+++
@@ -1,9 +0,0 @@
-module.exports.audify = function(object){
-  object.createdAt = {type: Date, required: true};
-  object.modifiedAt = {type: Date, required: true};
-  object.createdBy = {type: String, required: true};
-  object.modifiedBy = {type: String, required: true};
-  object.active = {type: Boolean, required: true, default: true};
-
-  return object;
-}
--- models/connector.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(mongoose, env){
-  var ur = "";
-  url = ur.concat("mongodb://",env.host,":",env.port,"/",env.database);
-  console.log(url);
-  mongoose.connect(url);
-}
